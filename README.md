0a9dba0310c5cc01712ab1b16227ec8133f3700e
Ivan Magda <imagda15@gmail.com>
2018-06-07 11:29:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ifmo-swift-static-analyzer
---
+++ ifmo-swift-static-analyzer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ifmo-swift-static-analyzer
+//
+//  Created by Ivan Magda on 07/06/2018.
+//  Copyright © 2018 Ivan Magda. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
